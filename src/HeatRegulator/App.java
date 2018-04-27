c33b042f0dd894bf5c4b65fb1700798cd83fb4b5
Grant Apodaca and Justin Hohman <pair+gapodaca+jhohman@pivotal.io>
2018-04-27 14:36:41 -0400

    Initial commit

---
+++ src/HeatRegulator/App.java
@@ -0,0 +1,26 @@
+import HeatRegulator.Oven;
+
+public class App {
+    public static void main(String args[]) {
+        new App().regulateTemp();
+    }
+    private void regulateTemp() {
+        int goalT, t, s;
+
+        while(true) {
+            Oven o = new Oven();
+
+            goalT = o.in(0x02);
+            t = o.in(0x01);
+            s = o.in(0x03);
+
+            if(s == 1) {
+                if(t < goalT) {
+                    o.out(0x04, true);
+                } else {
+                    o.out(0x04, false);
+                }
+            }
+        }
+    }
+}
